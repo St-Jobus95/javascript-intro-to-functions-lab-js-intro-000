13e790868bf2c9bfc295ade2ff19dc4c72f602df
Joseph Mawson <jmawson95@gmail.com>
2017-09-24 08:53:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase())
 }
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string = string.toLowerCase()) {
+    console.log('I can\'t hear you!')
+  } else if (string = string.toUpperCase()) {
+    console.log('YES INDEED!')
+  } else if (string = 'I love you, Grandma.') {
+    return 'I love you, too.'
+  }
+}
